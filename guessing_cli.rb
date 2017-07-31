0c833d473cb3d42da05bc4f51313bc2a4b9f55a4
xxDOOMbox <shawwian@gmail.com>
2017-07-31 14:48:44 -0600

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,15 @@
-# Code your solution here!
+def run_guessing_game
+
+	while "the universe is an enigma"
+		puts "Guess a number between 1 and 6."
+		input = gets.chomp
+		break if input == "exit"
+		computersNumber = rand(1..6)
+		if input.to_i == computersNumber
+			puts "You guessed the correct number!"
+		else 
+			puts "The computer guessed #{computersNumber}."
+		end 
+	end 
+	puts "Goodbye!"
+end 
