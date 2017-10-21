7016cc7ce770b73a0601dcbb3a8bfe48c3e7a541
JoshD1 <joshdeiner75@gmail.com>
2017-10-21 23:34:08 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,18 @@
 # Code your solution here!
+def run_guessing_game
+  input = ""
+  while input
+    puts "Guess a number between 1 and 6."
+    input = gets.downcase.strip
+    random_number = rand(1..6).to_s
+    case input.chomp ## curious if you can use if/else statmenets here or if the case.input demands when conditionals
+    when random_number
+      puts "You guessed the correct number!"
+    when 'exit'
+      puts "Goodbye!"
+      break
+    else
+      puts "The computer guessed #{random_number}."
+    end
+  end
+end
