dbe1e8caf6b298bd0767125cff1b0251dbae56d6
Christopher Diep <diep.christopher@gmail.com>
2018-01-05 08:24:18 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     if user_input == number
       puts "You guessed the correct number!"
     else
-      puts "The computer guessed #{number}"
+      puts "The computer guessed #{number}."
     end
   end
   puts "Goodbye!"
