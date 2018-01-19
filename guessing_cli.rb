9932913ab79c862a0a3e081390177dd2ba4cce6b
Mohammad Khan <sparrowangels@gmail.com>
2018-01-19 06:02:26 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,25 @@
 # Code your solution here!
+#
+def run_guessing_game
+  number = rand(1..6)
+  exit = false
+  # STDERR.puts "GOT HERE"
+  while exit == false
+    puts "Guess a number between 1 and 6."
+    response = gets.chomp
+    # response == number ? (puts "You guessed the correct number!") : (puts "The computer guessed #{number}") #only good for 2 items
+    if response == "exit"
+      
+      puts "Goodbye!"     #this line should be ADDED if we want a continous loop for game
+      exit = true
+    elsif response.to_i == number
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{number}."
+    end #if
+
+    # exit = true       #this line should be removed if we want a continous loop for game
+    # puts "Goodbye!"   #this line should be removed if we want a continous loop for game
+  end #while
+  # STDERR.puts "OUTHERE"
+end
