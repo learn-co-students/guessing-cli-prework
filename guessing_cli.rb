72b3fd180e9dd790dcb1d28139bfa09e1e651c34
Barney Fabian <bafa@tutanota.de>
2018-01-27 19:13:07 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   random = rand(10)
   puts "Enter number: "
   number = gets.chomp
-  puts "You guessed the correct number!" if number.to_i == random
+  puts "You guessed the correct number!" if number.to_i == random && number.to_s != "exit"
   puts "The computer guessed #{random}." if number.to_i != random && number != "exit"
   exit if number.to_s == "exit"
 end
