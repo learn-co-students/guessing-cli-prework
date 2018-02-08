5d1f01aff14669b1f826f21051add2c94080425c
Ray Blasko <mray6288@gmail.com>
2018-02-08 10:10:30 -0600

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,13 @@
-# Code your solution here!
+def run_guessing_game
+    input = ''
+    while input != exit
+        input = gets.chomp
+        rand_num = rand(10)
+        if input == rand_num
+            puts 'You guessed the correct number!'
+            break
+        else
+            puts "The correct number was #{rand_num}"
+        end
+    end
+end
