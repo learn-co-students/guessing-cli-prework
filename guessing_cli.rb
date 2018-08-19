576515acba904d7a4fa4b4d28d17e10bd0894657
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-19 18:13:35 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,6 +3,7 @@ require "pry"
 def run_guessing_game
   input = gets.chomp 
   if input = "exit"
+    break
   elsif input.to_i == computer_guessed_number
     puts "You guessed the correct number!"
   else puts "The computer guessed #{computer_guessed_number}."
