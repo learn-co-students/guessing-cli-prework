0097d7d959e88451114ae500b38067b54552616b
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-19 22:01:16 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
 input = ""
   while input != "exit"
     puts "Guess a number between 1 and 6."
-    input = gets.downcase.chomp
+    input = gets.chomp
     random_number = rand(1..6).to_s
   if input == "exit"
     puts "Goodbye!"
