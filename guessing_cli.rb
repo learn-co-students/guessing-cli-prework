1ac54bc5cb899c86fc676d482a033b8b78f416e8
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-19 20:31:35 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,15 +1,13 @@
 require "pry"
 def run_guessing_game
+  random_number = rand(1..6)
   puts "Guess a number between 1 and 6."
-  input = gets.chomp
-  while input != "exit"
-    random_number = rand(1..6).to_i 
-    if input == random_number
-      puts "You guessed the correct number!"
-    else puts "The computer guessed #{random_number}."
-    end
-    break if input == "exit"
+  input = gets.chomp 
+  if input == "exit"
     puts "Goodbye!"
+  elsif input == random_number.to_i 
+    puts "You guessed the correct number!"
+  else puts "The computer guessed #{random_number}."
   end 
 #    binding.pry
 end
