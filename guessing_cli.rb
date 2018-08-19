5534d9af2d5cf089d416a8805f275057d16a02b3
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-19 18:11:50 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,9 +1,8 @@
 def run_guessing_game
   input = gets.chomp 
-  if input == computer_guessed_number
+  if input = "exit"
+  elsif input.to_i == computer_guessed_number
     puts "You guessed the correct number!"
   else puts "The computer guessed #{computer_guessed_number}."
-  else  input == exit 
-    break
   end   
 end
