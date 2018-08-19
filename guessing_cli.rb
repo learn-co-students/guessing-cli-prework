e4266a31855cc8aad2b67eaf9a7e49cbeff51d67
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-19 21:54:21 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def run_guessing_game
 input = ""
-  while input
+  while input != "exit"
     puts "Guess a number between 1 and 6."
     input = gets.downcase.chomp
     random_number = rand(1..6).to_s
@@ -12,5 +12,6 @@ input = ""
     puts "You guessed the correct number!"
   else puts "The computer guessed #{random_number}."
   end 
+end 
 end
 
