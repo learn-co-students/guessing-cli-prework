f9055b0f3ac61b99e7fc6e31a19568145783e808
barbaradecares <github email address>
2018-09-10 13:12:35 -0800

    Guessing cli prework

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,21 @@
-# Code your solution here!
+def run_guessing_game
+  loop do
+  puts "Guess a number between 1 and 6."
+
+  userinput = gets.chomp
+  random = rand(1..6)
+
+  if userinput == "exit"
+    puts "Goodbye!"
+    break
+
+  elsif userinput.to_i == random
+    puts "You guessed the correct number!"
+
+  elsif userinput.to_i != random
+    puts "The computer guessed #{random}."
+  else
+    puts "Invalid input"
+  end
+end
+end
