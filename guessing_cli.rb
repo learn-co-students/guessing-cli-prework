91196e8fd36f088e3115453e4c6ed841c3f8713d
Asaf Davidov <adavidov@conncoll.edu>
2018-09-13 02:14:32 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -11,13 +11,14 @@ def compare(given,random)
   if given == random
     puts "You guessed the correct number!"
   else
-    puts "The computer guessed #{random}"
+    puts "The computer guessed #{random}."
   end
 end
 
 def run_guessing_game
   exitting = false
   puts "Guess a number between 1 and 6."
+  
   while exitting != true
     user_guess = gets.chomp
     if user_guess == 'exit'
@@ -25,7 +26,7 @@ def run_guessing_game
       exitting = true
     else  
       user_guess_int = user_guess.to_i
-      random_int = 1 + rand(6)
+      random_int = rand(1..6)
       compare(user_guess_int, random_int)
     end
   end
