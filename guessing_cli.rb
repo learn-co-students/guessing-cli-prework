fb10cb4ff5952a7009bcb15f4331d29e03338ab6
leannemcabey <leanne.m.cabey@gmail.com>
2018-10-16 14:10:42 -0400

    completed #run_guessing_game method to pass all tests

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,17 @@
-# Code your solution here!
+def run_guessing_game
+  puts "Guess a number between 1 and 6:"
+  number = rand(1..6)
+  answer = gets.chomp
+  until answer == "exit"
+    if answer.to_i == number
+      puts "You guessed the correct number!"
+      answer = gets.chomp
+      number = rand(1..6)
+    else
+      puts "The computer guessed #{number}."
+      answer = gets.chomp
+      number = rand(1..6)
+    end
+  end
+  puts "Goodbye!"
+end
